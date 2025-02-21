95f0411b95b5974a7c31f80fbe1d14a94cf7a41f
JulRiv07 <Julian.rivera@utp.edu.co>
2025-02-21 09:14:24 -0500

    Nuevo

--- Primer_Corte/Vectores.cc
+++ Primer_Corte/Vectores.cc
@@ -1,66 +1,112 @@
-#include <iostream>
-#include <vector>
-#include <random>
-#include <string>
-#include <algorithm>
-
+#include <iostream> 
+#include <cassert>
 using namespace std;
 
-vector<int> generateRandomArray(int size){
+template <typename T>
 
-    // Obtain a random seed from the operating system's entropy source.
-    random_device rd;
-    // Standard mersenne_twister_engine seeded with rd().
-    mt19937 gen(rd());
+class Vector{
+    private:
 
-    // Generate random numbers between 0 and 100 (inclusive).
-    uniform_int_distribution<> distrib(0, 100);
-    
-    // Create a vector of the specified size.
-    vector<int> randomArray(size);
+    T* storage_;
+    unsigned int capacity_;
+    unsigned int size_;
 
-    for(int i = 0; i < size; ++i){
-        randomArray[i] = distrib(gen); //Generate and store random number 
+    void resize(){
+        cout << "Resize" << endl;
+        unsigned int capacity2 = capacity_ * 1.5;
+        T* storage2 = new T[capacity2];
+        for(unsigned int i = 0; i < size_; i++){
+            storage2[i] = storage_[i];
+        }
+
+        delete[] storage_;
+        storage_ = storage2;
+        capacity_ = capacity2;
     }
 
-    return randomArray;
-}
+    public:
+    Vector(){
+        capacity_ = 5;
+        storage_ = new T[capacity_];
+        size_ = 0;
+    }
 
-string generateRandomString(int size) {
-    string randomString(size, ' '); // Initialize with spaces
+    Vector(unsigned int c, T elemen){
+        capacity_ = c;
+        storage_ = new T[capacity_];
+        for(unsigned int i = 0; i < capacity_; i++){
+            storage_[i] = elemen;
+        }
+        size_ = capacity_;
+    }
 
-    random_device rd;
-    mt19937 gen(rd());
-    uniform_int_distribution<> distrib('A', 'z'); // Characters from A to z (including uppercase and lowercase)
+    unsigned int size(){
+        return size_;
+    }
 
-    // Use std::generate to fill the string with random characters
-    generate(randomString.begin(), randomString.end(), [&]() { return distrib(gen); });
+    T& at(unsigned int pos) {
+        assert(pos >= 0 && pos < size_);
+        return storage_[pos];
+    }
 
-    return randomString;
+    const T& at(unsigned int pos) const {
+        assert(pos >= 0 && pos < size_);
+        return storage_[pos];
+        
+    }
+
+    const T& operator[](unsigned int index) const{
+        return storage_[index];
+    }
+
+    void push_back(const T& elem) {
+        if(size_ == capacity_){
+            resize();
+        }
+        storage_[size_] = elem;
+        size_++;
+    }
+
+    void pop_back(){
+        size_--;
+    }
+
+    void print(){
+        for(unsigned int i = 0; i < size_; i++){
+            cout << " " << storage_[i];
+        }
+        cout << endl;
+    }
+};
+
+class complex {
+    private:
+    double real;
+    double img;
+
+    public:
+        complex(){
+           real = 0.0;
+           img = 0.0; 
+        }
+        friend std::ostream& operator<<(std::ostream& os, const complex& c);
+
+};
+
+std::ostream& operator<<(std::ostream& os, const complex& c){
+    os <<"(" << c.real << ", " << c.img << "i";
+    return os;
 }
 
 int main(){
-    cout << "Vector examples: " << endl;
-    vector<int> a;
-    cout << "Size: " << a.size() << endl;
-    cout << "Vector examples: " << endl;
-    vector<int> b;
-    b = generateRandomArray(100);
 
-    for (int i = 0; i < b.size(); i++){
-        cout << " " << b.at(i); 
-    }
+    cout << "Ejecutando vectores!" << endl;
 
-    cout << "Vector examples: " << endl;
-    vector<string> cadenas;
-
-    cout << endl;
-
-    for (int i = 0; i < 100; i++){
-        cadenas.push_back(generateRandomString(5));
-    }
-
-    cout << endl;
+    Vector<int> x(10, 0);
+    cout << "x " << x.size() << endl;
+    x.print();
+    x.at(5) = 100;
+    x.print();
 
     return 0;
 }
