529b0edc60aa465f55b2ac574c76f081ddcdc59f
JulRiv07 <Julian.rivera@utp.edu.co>
2025-05-10 22:52:00 -0500

    Dot product function

---
+++ Tercer corte/dot_product.cc
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int docprod(const vector<int> &a, const vector<int> &b){
+    int result = 0;
+
+    for(int i = 0; i < a.size(); i++){
+        result += a[i] * b[i];
+    }
+
+    return result;
+}
+
+int main(){
+
+    int n;
+
+    cout << "Hi! if you want calculate the dot product your vectors must have the same cardinality..."
+          << "So, how many elements you want to add in the vectors?:";
+
+    cin >> n;
+
+    vector<int> vec1(n);    
+    cout << "Add the elements to vec1 (1 for 1)" << endl;
+    for(int i = 0; i < n; i++){
+        cout << "Enter the element " << i + 1 << ":" ;
+        cin >> vec1[i];
+    }
+
+    vector<int> vec2(n);   
+    cout << "Add the elements to vec2 (1 for 1)" << endl;
+    for(int i = 0; i < n; i++){
+        cout << "Enter the element " << i + 1 << ":" ;
+        cin >> vec2[i];
+    }
+
+    cout << "The dot product between (vec1) y (vec2) es: " << docprod(vec1, vec2) << endl;
+
+    return 0;
+}
