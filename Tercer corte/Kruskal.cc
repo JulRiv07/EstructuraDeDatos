11c4e75059efff7657c0bef5661ff9dbb9d30ac0
JulRiv07 <Julian.rivera@utp.edu.co>
2025-05-21 10:20:59 -0500

    Kruskal finish

--- Tercer corte/Kruskal.cc
+++ Tercer corte/Kruskal.cc
@@ -1,49 +1,47 @@
-#include <iostream>   
-#include <vector>     
-#include <algorithm>  
+#include <iostream>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
 class Edge {
 public:
-    int src, dest;   // Nodo origen y destino
-    int weight;      // Peso de la arista
+    int src, dest;   
+    int weight;       
 
     Edge(int s, int d, int w) : src(s), dest(d), weight(w) {}
 
-    // Para ordenar aristas por peso
     bool operator<(const Edge& other) const {
         return weight < other.weight;
     }
 };
-
-
 class DisjointSet {
 private:
-    vector<int> parent, rank;
+    vector<int> parent;  
+    vector<int> rank;    
 
 public:
-    // Constructor inicializa N sets disjuntos (cada nodo es su propio padre)
+
     DisjointSet(int n) {
         parent.resize(n);
         rank.resize(n, 0);
-        for (int i = 0; i < n; i++) parent[i] = i;
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+        }
     }
 
-    // Encuentra el padre (representante) del set con path compression
     int find(int u) {
         if (parent[u] != u)
             parent[u] = find(parent[u]);
         return parent[u];
     }
 
-    // Une dos sets si son diferentes, usando union por rango
     bool unite(int u, int v) {
         int rootU = find(u);
         int rootV = find(v);
 
         if (rootU == rootV)
-            return false; // Ya están en el mismo set (ciclo)
+            return false; 
 
         if (rank[rootU] < rank[rootV])
             parent[rootU] = rootV;
@@ -59,53 +57,59 @@ public:
 
 class Graph {
 private:
-    int V;                  // Número de vértices
-    vector<Edge> edges;     // Lista de aristas
+    int V;                  
+    vector<Edge> edges;     
 
 public:
-    Graph(int vertices) : V(vertices) {}
 
-    // Agrega una arista
-    void addEdge(int src, int dest, int weight) {
-        edges.push_back(Edge(src, dest, weight));
+    Graph(int vertices) {
+        V = vertices ;
     }
 
-    // Ejecuta el algoritmo de Kruskal para encontrar MST
-    vector<Edge> kruskalMST() {
-        vector<Edge> result;      // Aristas del MST
-        DisjointSet ds(V);
+    void addEdge(int src, int dest, int weight) {
+        edges.emplace_back(src, dest, weight);
+    }
 
-        // Ordena aristas por peso ascendente
+    vector<Edge> kruskalMST() {
+        vector<Edge> mst;           
+        DisjointSet ds(V);          
+
         sort(edges.begin(), edges.end());
 
-        for (Edge& e : edges) {
-            if (ds.unite(e.src, e.dest)) {
-                result.push_back(e);
-                if (result.size() == V - 1)
-                    break;  // MST completo
+        for (Edge& edge : edges) {
+            if (ds.unite(edge.src, edge.dest)) {
+                mst.push_back(edge);
+                if ((int)mst.size() == V - 1)
+                    break;
             }
         }
 
-        return result;
+        return mst;
+    }
+
+    void printMST(const vector<Edge>& mst) {
+        int totalWeight = 0;
+        cout << "Edges in the MST:\n";
+        for (const Edge& e : mst) {
+            cout << e.src << " -- " << e.dest << " == " << e.weight << "\n";
+            totalWeight += e.weight;
+        }
+        cout << "Total weight of MST: " << totalWeight << "\n";
     }
 };
 
 int main() {
-    Graph g(4); // Grafo con 4 nodos (0 a 3)
+    int vertices = 1000;
+    Graph graph(vertices);
 
-    // Agregar aristas: (origen, destino, peso)
-    g.addEdge(0, 1, 10);
-    g.addEdge(0, 2, 6);
-    g.addEdge(0, 3, 5);
-    g.addEdge(1, 3, 15);
-    g.addEdge(2, 3, 4);
-
-    vector<Edge> mst = g.kruskalMST();
-
-    cout << "Aristas del MST:\n";
-    for (const Edge& e : mst) {
-        cout << e.src << " -- " << e.dest << " == " << e.weight << "\n";
+    for (int i = 0; i < vertices - 1; i++) {
+        graph.addEdge(i, i + 1, rand() % 100 + 1);  
+        if (i + 10 < vertices)
+            graph.addEdge(i, i + 10, rand() % 100 + 1);
     }
+
+    vector<Edge> mst = graph.kruskalMST();
+    graph.printMST(mst);
 
     return 0;
 }
